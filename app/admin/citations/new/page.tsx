1b690f89f94bdbb150e5b41e1fd05bab3e809684
elias <emedim.araneda@gmail.com>
2025-03-01 17:51:57 +0100

    adding prisma.db and page.tsx and route.ts

--- app/admin/citations/new/page.tsx
+++ app/admin/citations/new/page.tsx
@@ -1,20 +1,41 @@
+"use client";
+
 import { Button } from "@/src/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/src/components/ui/card";
 import { Input } from "@/src/components/ui/input";
 import { Label } from "@/src/components/ui/label";
+import { useState } from "react";
 //import Form from "next/form";
 
 
 export default function Page() {
+    const [isLoading, setIsLoading] = useState(false)
+
+    const createCitation = async (FormData: FormData) => {
+        const result = await fetch(`/api/citations`, {
+            body: JSON.stringify({
+                citation: FormData.get("citation"),
+                author: FormData.get("author"),
+            }),
+            method: "POST",
+        });
+        const json = await result.json();
+        setIsLoading(false);
+
+        console.log(json);
+    }
+
     return (
         <Card>
             <CardHeader>
                 <CardTitle>Create citation</CardTitle>
             </CardHeader>
-            <CardContent className="flex flex-col gap-2">
-        <form action="/api/citations" 
-        method="post"
-        className="flex flex-col gap-2"
+            <CardContent>
+                <form 
+                action={async (formData) => {
+                    createCitation(formData);
+                }} 
+                className="flex flex-col gap-2"
         >
             <Label>
                 Citation
@@ -24,7 +45,9 @@ export default function Page() {
                 Author
             <Input  name="author" />
             </Label>
-            <Button type="submit">Submit</Button>
+            <Button disabled={isLoading} type="submit">
+                {isLoading ? "Loading..." : "Submit"}
+            </Button>
         </form>
         </CardContent>
         </Card>
